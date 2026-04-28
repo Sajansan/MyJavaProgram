19ef0438678df2f00e193a7da3731afd7a610b67
Sajansan <sajansan47@gmail.com>
2026-04-28 13:18:35 +0530

    Random number practice

--- src/RandomNumber.java
+++ src/RandomNumber.java
@@ -1,8 +1,15 @@
 import java.util.Random;
 
 public class RandomNumber {
-    public static void main(String[] args){
-        Random ran = new Random();
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        //int x = rand.nextInt(6) + 1;
+        //double y = rand.nextDouble(5);
+
+        boolean z = rand.nextBoolean();
+
+        System.out.println(z);
 
     }
 }
