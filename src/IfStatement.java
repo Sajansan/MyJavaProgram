5b0b3c69d98ab6c87afd8608a867d55666b74192
Sajansan <sajansan47@gmail.com>
2026-04-29 11:28:57 +0530

    If statement practiced

---
+++ src/IfStatement.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class IfStatement {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = scan.nextInt();
+
+        if (age >= 18) {
+            System.out.println("You are an adult!");
+        } else if (age >= 70) {
+            System.out.println("Ok Boomer!");
+        } else {
+            System.out.println("You are a chick");
+        }
+    }
+}
