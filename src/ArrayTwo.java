55fca93387ed688f1b09b5bd10428268d9f82062
Sajansan <sajansan47@gmail.com>
2026-05-05 08:57:50 +0530

    Array practice

---
+++ src/ArrayTwo.java
@@ -0,0 +1,13 @@
+public class ArrayTwo {
+    public static void main(String[] args) {
+
+        String[] fruits = new String[4];
+
+        fruits[0] = "Apple";
+        fruits[1] = "Banana";
+        fruits[2] = "Orange";
+        fruits[3] = "Grapes";
+
+        System.out.println(fruits[1]);
+    }
+}
---
+++ src/ArraysStart.java
@@ -0,0 +1,11 @@
+public class ArraysStart {
+    public static void main(String[] args) {
+        // array = used to store multiple values in a single variable
+
+        String[] cars = {"BMW", "Audi", "Range Rover","Ferrari" };
+
+        cars[1] = "Lamborghini";
+
+        System.out.println(cars[3]);
+    }
+}
