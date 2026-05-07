ea1c8a017de50169ac5880b07678c6d9a445baf9
Sajansan <sajansan47@gmail.com>
2026-05-07 13:08:37 +0530

    Wrapper class practiced in Java

---
+++ src/WrapperClass.java
@@ -0,0 +1,16 @@
+public class WrapperClass {
+    public static void main(String[] args) {
+        // wrapper class = provides a way to use primitive data types as reference data types
+        //                 reference data types contain useful methods
+        //                 can be used with collections (ex.ArrayList)
+
+        Boolean a = true;
+        Character b = '@';
+        Integer i = 123;
+        Double d = 3.14;
+
+        if (b == '@') {
+            System.out.println("This is fucking true");
+        }
+    }
+}
